f885a9f54bc42be4e56df103fe20cc36121723de
Pascal Koeiman <19142617@student.hhs.nl>
2023-09-26 10:10:57 +0200

    Create initial migration and add PasskeySecurity bool

---
+++ WebApplication2/Migrations/20230918084130_AddPasskeySecurityToApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasskeySecurityToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PasskeySecurityEnabled",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasskeySecurityEnabled",
+                table: "AspNetUsers");
+        }
+    }
+}
