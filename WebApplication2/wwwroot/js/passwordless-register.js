58fe9d47125f0e166594870f50bdbe8dafa40082
Pascal Koeiman <19142617@student.hhs.nl>
2023-09-26 10:18:49 +0200

    Add javascript for passwordless client and register function

---
+++ WebApplication2/wwwroot/js/passwordless-register.js
@@ -0,0 +1,22 @@
+﻿async function RegisterPasswordless(e) {
+    e.preventDefault();
+
+    const Client = Passwordless.Client;
+    var p = new Client();
+
+    // Call the backend to get a passwordless token
+    var myToken = await fetch("/passwordless/getregistertoken").then(r => r.text());
+
+    try {
+        // See passwordless-client.js for the method implementations
+        const result = await p.register(myToken);
+        console.log(result);
+
+        // Refresh page
+        window.location.href = "/";
+    } catch (e) {
+        console.error("Things went bad: ", e);
+    }
+}
+
+document.getElementById('passwordless-enable').addEventListener('click', RegisterPasswordless);
